f5efc49fe6debb340e30668b8e4028c0b3e7141b
Sarvjeet Rajvansh <rajvanshsarvjeet@gmail.com>
2022-07-12 00:51:54 +0530

    adding first code

--- hello.py
+++ hello.py
@@ -1,11 +1,7 @@
-lst = []
 
-for i in range(5) :
-    inp = input("Enter a string: ")
-    lst.append(inp)
-print(lst)
-result=""    
-for j in range(3) :
-    inpt = int(input("Enter a number: "))
-    result+=lst[inpt]
-print(result)
+def greeter(name) :
+    print("Hello!",name)
+    
+name = input("Enter your name : ")
+
+greeter(name)
